60d8fae6e5a58f377d042feb2cfbf2d8da02833c
TheSims0 <mahtynas@gmail.com>
2023-04-27 16:15:58 +0300

    Skaiciuotuvas

---
+++ Skaiciuotuvas/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Skaiciuotuvas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius1 = 1;
+            int skaicius2 = 3;
+        }
+        
+    }
+    public class Skaiciuotuvas
+    {
+        public static double Dalyba(int skaicius1, int skaicius2)
+        {
+            return (double)skaicius1 / skaicius2;
+        }
+        public static double SandaugaDu(double skaicius1, double skaicius2)
+        {
+            return skaicius1 * skaicius2;
+        }
+        public static double SudetisDu(double skaicius1, double skaicius2)
+        {
+            return skaicius1 + skaicius2;
+        }
+        public static decimal SandaugaDecimal(decimal skaicius1, decimal skaicius2)
+        {
+            return skaicius1 * skaicius2;
+        }
+        public static decimal SudetisDecimal(decimal skaicius1, decimal skaicius2)
+        {
+            return skaicius1 + skaicius2;
+        }
+    }
+}
